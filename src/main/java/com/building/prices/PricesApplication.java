7a04aaca184f0445d483ae2ff9f95d4a5cc7be9f
林勇鹏 <linyp@travelsky.com>
2020-11-02 14:16:44 +0800

    clear

--- src/main/java/com/building/prices/PricesApplication.java
+++ src/main/java/com/building/prices/PricesApplication.java
@@ -3,10 +3,8 @@ package com.building.prices;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.building.prices.*")
 @MapperScan("com.building.prices.mapper")
 public class PricesApplication {
     public static void main(String[] args) {
--- src/main/java/com/building/prices/mapper/InfoMapper.java
+++ src/main/java/com/building/prices/mapper/InfoMapper.java
@@ -1,9 +1,8 @@
 package com.building.prices.mapper;
 
-import com.building.prices.entity.Info;
-import java.util.List;
+
 
 public interface InfoMapper {
 
-    List<Info> findAll();
+
 }
